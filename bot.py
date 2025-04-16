016dae0b9d1a66a3a669beeacd2570268ce6b656
Artem Agaev <aaaaa>
2025-04-16 15:42:54 +0500

    Add bot poilerplate

--- bot.py
+++ bot.py
@@ -1,2 +1,22 @@
+import asyncio
+import logging
+import sys
+import config
+
+from aiogram import Bot, Dispatcher, html
+
+TOKEN=config.TOKEN
+print(TOKEN)
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
-    print("Started!")
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
