6ad0c5f48826bfae1f63a71dc5186cdea3d0811d
Artem Agaev <aaaaa>
2025-04-16 11:53:27 +0500

    Remove py util from install

--- install.py
+++ install.py
@@ -2,5 +2,5 @@ import os, pathlib
 
 if __name__ == "__main__":
     project_root = pathlib.Path(__file__).parent.resolve()
-    os.system('py -3.13 -m venv ./venv')
+    os.system('python3.13 -m venv ./venv')
     os.system(f'{project_root}/venv/Scripts/pip.exe install -r requirements.txt')
