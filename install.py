91c2eb7382d55399d94920b4ae3bbbf9271dec61
Artem Agaev <aaaaa>
2025-04-16 12:26:11 +0500

    Add linux to install

--- install.py
+++ install.py
@@ -1,6 +1,10 @@
-import os, pathlib
+import os, pathlib, platform
 
 if __name__ == "__main__":
     project_root = pathlib.Path(__file__).parent.resolve()
     os.system('python3.13 -m venv ./venv')
-    os.system(f'{project_root}/venv/Scripts/pip.exe install -r requirements.txt')
+    match platform.system():
+        case 'Windows':
+            os.system(f'{project_root}/venv/Scripts/pip.exe install -r requirements.txt')
+        case 'Linux':
+            os.system(f'{project_root}/venv/bin/pip install -r requirements.txt')
