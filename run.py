e3465c836ab62e25c56dab5ab9804ce4ff3e9b2d
Artem Agaev <aaaaa>
2025-04-16 12:21:19 +0500

    Add linux to run

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-import os, pathlib
+import os, pathlib, platform
 
 if __name__ == "__main__":
     project_root = pathlib.Path(__file__).parent.resolve()
-    os.system(f'{project_root}/venv/Scripts/python.exe {project_root}/bot.py')
+    match platform.system():
+        case 'Windows':
+            os.system(f'{project_root}/venv/Scripts/python.exe {project_root}/bot.py')
+        case 'Linux':
+            os.system(f'{project_root}/venv/bin/python3 {project_root}/bot.py')
